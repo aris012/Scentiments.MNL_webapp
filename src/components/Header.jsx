a55af61e69e21b7588077de69e4a91d5a94fd593
Aris <165860478+aris012@users.noreply.github.com>
2024-12-05 11:02:20 +1300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "../style/header.css";
+import { useNavigate } from "react-router-dom";
+const Header = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/product");
+  };
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Scentiments.MNL
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarScroll">
+          <ul
+            className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
+            style={{ "--bs-scroll-height": "100%" }}
+          >
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">
+                About
+              </a>
+            </li>
+
+            <li className="nav-item dropdown">
+              <a
+                className="drop-nav nav-link dropdown-toggle"
+                role="button"
+                data-bs-toggle="dropdown"
+              >
+                Product
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="/product">
+                    Premium Candle 
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/product1">
+                    History Collection Candle
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/product2">
+                    Personalized Message Candle
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/product2">
+                    Limited Edition
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
